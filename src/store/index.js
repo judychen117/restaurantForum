124571a75c9ae88a02e436ec6b68daefe52596d9
judy <judychen117@gmail.com>
2021-03-04 12:59:22 +0800

    Q1

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,9 @@ import usersAPI from './../apis/users'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  // 存放資料，相當於data
   state: {
     currentUser: {
-      id: undefined,
+      id: -1,
       name: '',
       email: '',
       image: '',
@@ -16,26 +15,24 @@ export default new Vuex.Store({
     },
     isAuthenticated: false
   },
-  // 用來操作data的函式庫，把api接回的資料放入等等...
   mutations: {
-    // 兩個參數： state: 上方的state, currentUser: 待傳入的currentUser用來更新state的資料
-    setCurrentUser(state, currentUser) {
+    setCurrentUser (state, currentUser) {
       state.currentUser = {
         ...state.currentUser,
-        ...currentUser
+        // 將 API 取得的 currentUser 覆蓋掉 Vuex state 中的 currentUser
+        ...currentUser 
       }
+      // 將使用者的登入狀態改為 true
       state.isAuthenticated = true
     }
   },
-  // 向 API fetch data 專用的函式庫
   actions: {
-    async fetchCurrentUser({ commit }) {
+    async fetchCurrentUser ({ commit }) {
       try {
         const { data } = await usersAPI.getCurrentUser()
-        if (data.status === 'error') {
-          throw new Error(data.message)
-        }
+
         const { id, name, email, image, isAdmin } = data
+
         commit('setCurrentUser', {
           id,
           name,
@@ -43,12 +40,11 @@ export default new Vuex.Store({
           image,
           isAdmin
         })
-      } catch(e) {
-        console.log(e)
+      } catch (error) {
+        console.error(error.message)
       }
     }
   },
-  // 存放模組，vuex 規模龐大時用來拆檔
   modules: {
   }
 })
